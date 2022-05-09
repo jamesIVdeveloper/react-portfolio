57b2ce6dabb849164152ec621b19cb3aa004b809
James Murphy <jamesivdeveloper@gmail.com>
2022-05-09 13:22:09 -0500

    set-up Nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 import About from "./components/About";
+import Nav from "./components/Nav";
 
 function App() {
+  const [pages] = useState([
+    {
+      name: "Portfolio",
+    },
+    {
+      name: "Resume",
+    },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
     <div>
+      <Nav
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
       <main>
         <About />
       </main>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Nav() {
+function Nav(props) {
+  const { pages = [], setCurrentPage, currentPage } = props;
+
+  useEffect(() => {
+    document.title = currentPage.name;
+  }, [currentPage]);
+
   return (
     <header>
+      <h2>
+        <a href="/">James</a>
+      </h2>
       <nav>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a href="#about">About me</a>
+          </li>
+          {pages.map((page) => (
+            <li
+              className={`${currentPage.name === page.name && `navActive`}`}
+              key={page.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentPage(page);
+                }}
+              >
+                {page.name}
+              </span>
+            </li>
+          ))}
+          <li>
+            <span>Contact</span>
+          </li>
         </ul>
       </nav>
     </header>
   );
 }
+
+export default Nav;
