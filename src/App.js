77eb6c406ac340fd2235a5df51cdeca678aed362
James Murphy <jamesivdeveloper@gmail.com>
2022-05-10 19:15:17 -0500

    style portfolio component

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       />
       <main>
         <About />
-        <Portfolio />
+        <Portfolio currentPage={currentPage} />
       </main>
     </div>
   );
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,75 +6,123 @@ function Portfolio() {
       <h1 id="portfolio-title">Portfolio</h1>
       <div id="projects">
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/90s.png")}
+            alt="landing page of my 90's movies reviews application"
+          />
           <a
             href="https://itwasthe90s.herokuapp.com/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/90s.png")}
-              alt="landing page of my 90's movies reviews application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/Jasonwesleysmith/it-was-the-90s"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/runbuddy.png")}
+            alt="landing page of my run buddy application"
+          />
           <a
             href="https://jamesivdeveloper.github.io/run-buddy/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/runbuddy.png")}
-              alt="landing page of my run buddy application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/jamesIVdeveloper/run-buddy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/hangman.png")}
+            alt="landing page of hangman game application"
+          />
           <a
             href="https://shawnmwillard.github.io/Hangman-Game/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/hangman.png")}
-              alt="landing page of hangman game application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/shawnmwillard/Hangman-Game"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/tigerwoods.png")}
+            alt="landing page of my tiger woods quiz application"
+          />
           <a
             href="https://jamesivdeveloper.github.io/Tiger-Quiz-App/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/tigerwoods.png")}
-              alt="landing page of my tiger woods quiz application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/jamesIVdeveloper/Tiger-Quiz-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/parkfinder.png")}
+            alt="landing page of my park finder application"
+          />
           <a
             href="https://jamesivdeveloper.github.io/national-park/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/parkfinder.png")}
-              alt="landing page of my park finder application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/jamesIVdeveloper/national-park"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
         <div class="project">
+          <img
+            src={require("../../assets/project-imgs/gamehub.png")}
+            alt="landing page of my game hub application"
+          />
           <a
             href="https://jamesivdeveloper.github.io/game-hub/"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              src={require("../../assets/project-imgs/gamehub.png")}
-              alt="landing page of my game hub application"
-            />
+            Deployed
+          </a>
+          <a
+            href="https://github.com/jamesIVdeveloper/game-hub"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Repo
           </a>
         </div>
       </div>
