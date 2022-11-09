16a792f5c09eee0497b3b347063ef175393a3479
James Murphy <jamesivdeveloper@gmail.com>
2022-11-09 15:49:43 -0600

    working updates of portfolio component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,7 +5,137 @@ function Portfolio() {
   return (
     <section>
       <div id="projects">
-        <div class="project">
+        <div className="project">
+          <div className="project-info">
+            <h1 className="project-name">Tech Blog</h1>
+            <p className="project-description">
+              A blog site, where you can log in, create blogs, and add comments
+              on other users' blogs.
+            </p>
+            <div className="tools-container">
+              <h3 className="tools-title">Built With</h3>
+              <ul className="project-tools">
+                <li className="tool">JavaScript</li>
+                <li className="tool">Handlebars</li>
+                <li className="tool">MySQL</li>
+                <li className="tool">Express</li>
+                <li className="tool">Sequelize</li>
+              </ul>
+            </div>
+            <p>
+              Check out the{" "}
+              <a
+                href="https://intense-shelf-50188.herokuapp.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live
+              </a>{" "}
+              version or the{" "}
+              <a
+                href="https://github.com/jamesIVdeveloper/tech-blog"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </p>
+          </div>
+          <div className="project-img-container">
+            <img
+              className="project-img"
+              src={require("../../assets/project-imgs/tech-blog-landing.png")}
+              alt="landing page of my tech blog application"
+            />
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-info">
+            <h1 className="project-name">Check</h1>
+            <p className="project-description">
+              An app where you can organize your to-do list and toggle tasks
+              between 'On Deck', 'In Progress', and 'Done'.
+            </p>
+            <div className="tools-container">
+              <h3 className="tools-title">Built With</h3>
+              <ul className="project-tools">
+                <li className="tool">HTML</li>
+                <li className="tool">CSS</li>
+                <li className="tool">JavaScript</li>
+              </ul>
+            </div>
+            <p>
+              Check out the{" "}
+              <a
+                href="https://jamesivdeveloper.github.io/check/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live
+              </a>{" "}
+              version or the{" "}
+              <a
+                href="https://github.com/jamesIVdeveloper/check"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </p>
+          </div>
+          <div className="project-img-container">
+            <img
+              className="project-img"
+              src={require("../../assets/project-imgs/check-landing-page.png")}
+              alt="landing page of my tech blog application"
+            />
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-info">
+            <h1 className="project-name">Parkfinder</h1>
+            <p className="project-description">
+              An app where you can find national parks in any state, and select
+              how many you would like to be shown. This app uses the National
+              Park Service API.
+            </p>
+            <div className="tools-container">
+              <h3 className="tools-title">Built With</h3>
+              <ul className="project-tools">
+                <li className="tool">HTML</li>
+                <li className="tool">CSS</li>
+                <li className="tool">JavaScript</li>
+                <li className="tool">jQuery</li>
+              </ul>
+            </div>
+            <p>
+              Check out the{" "}
+              <a
+                href="https://jamesivdeveloper.github.io/national-park/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live
+              </a>{" "}
+              version or the{" "}
+              <a
+                href="https://github.com/jamesIVdeveloper/national-park"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub Repo
+              </a>
+            </p>
+          </div>
+          <div className="project-img-container">
+            <img
+              className="project-img"
+              src={require("../../assets/project-imgs/parkfinder.png")}
+              alt="landing page of my tech blog application"
+            />
+          </div>
+        </div>
+        {/* <div class="project">
           <img
             src={require("../../assets/project-imgs/tech-blog-landing.png")}
             alt="landing page of my tech blog application"
@@ -164,7 +294,7 @@ function Portfolio() {
           >
             GitHub Repo
           </a>
-        </div>
+        </div> */}
       </div>
     </section>
   );
